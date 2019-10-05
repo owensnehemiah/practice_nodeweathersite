19710bb4ff74a3e26af356ec02e3a6e2aeef2146
Neo Owens <nowens@partneresi.com>
2019-10-04 23:38:07 -0400

    set up map for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-// console.log('Client side javascript file is loaded!')
+// console.log('Client side javascript file is loaded!')ls -a
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -7,8 +7,8 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    const domain = 
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error)
